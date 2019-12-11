3909513c29acb3553002a838fa56da5b7827aec4
Nicos <nkasenides@uclan.ac.uk>
2019-12-11 18:33:01 +0200

    Added first version of Rover blocks

---
+++ client/rover-blocks/rover-block-codes.js
@@ -0,0 +1,129 @@
+Blockly.JavaScript['move_forward'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['turn_anticlockwise'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['turn_clockwise'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['direction_north'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['direction_south'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['direction_east'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['direction_west'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['scan_color'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_none'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_black'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_blue'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_green'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_yellow'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_red'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_white'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['color_brown'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['set_scan_distance'] = function(block) {
+  var value_distance = Blockly.JavaScript.valueToCode(block, 'distance', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['get_direction'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['scan'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
---
+++ client/rover-blocks/rover-block-definitions.js
@@ -0,0 +1,161 @@
+[{
+  "type": "move_forward",
+  "message0": "Move Forward",
+  "previousStatement": null,
+  "colour": 230,
+  "tooltip": "Move forward.",
+  "helpUrl": ""
+},
+{
+  "type": "turn_anticlockwise",
+  "message0": "Turn anti-clockwise",
+  "previousStatement": null,
+  "colour": 230,
+  "tooltip": "Turn anti-clockwise.",
+  "helpUrl": ""
+},
+{
+  "type": "turn_clockwise",
+  "message0": "Turn clockwise",
+  "previousStatement": null,
+  "colour": 230,
+  "tooltip": "Turn clockwise.",
+  "helpUrl": ""
+},
+{
+  "type": "direction_north",
+  "message0": "Direction.NORTH",
+  "output": "Direction",
+  "colour": 60,
+  "tooltip": "Direction North.",
+  "helpUrl": ""
+},
+{
+  "type": "direction_south",
+  "message0": "Direction.SOUTH",
+  "output": "Direction",
+  "colour": 60,
+  "tooltip": "Direction South.",
+  "helpUrl": ""
+},
+{
+  "type": "direction_east",
+  "message0": "Direction.EAST",
+  "output": "Direction",
+  "colour": 60,
+  "tooltip": "Direction East.",
+  "helpUrl": ""
+},
+{
+  "type": "direction_west",
+  "message0": "Direction.WEST",
+  "output": "Direction",
+  "colour": 60,
+  "tooltip": "Direction West.",
+  "helpUrl": ""
+},
+{
+  "type": "scan_color",
+  "message0": "Scan color",
+  "output": "Color",
+  "colour": 230,
+  "tooltip": "Scan color.",
+  "helpUrl": ""
+},
+{
+  "type": "color_none",
+  "message0": "Color.NONE",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color None.",
+  "helpUrl": ""
+},
+{
+  "type": "color_black",
+  "message0": "Color.BLACK",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color Black.",
+  "helpUrl": ""
+},
+{
+  "type": "color_blue",
+  "message0": "Color.BLUE",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color Blue.",
+  "helpUrl": ""
+},
+{
+  "type": "color_green",
+  "message0": "Color.GREEN",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color Green.",
+  "helpUrl": ""
+},
+{
+  "type": "color_yellow",
+  "message0": "Color.YELLOW",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color Yellow.",
+  "helpUrl": ""
+},
+{
+  "type": "color_red",
+  "message0": "Color.RED",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color Red.",
+  "helpUrl": ""
+},
+{
+  "type": "color_white",
+  "message0": "Color.WHITE",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color White.",
+  "helpUrl": ""
+},
+{
+  "type": "color_brown",
+  "message0": "Color.BROWN",
+  "output": "Color",
+  "colour": 60,
+  "tooltip": "Color Brown.",
+  "helpUrl": ""
+},
+{
+  "type": "set_scan_distance",
+  "message0": "Set scan distance %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "distance",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "Set the scan distance for the ",
+  "helpUrl": ""
+},
+{
+  "type": "get_direction",
+  "message0": "Get Direction",
+  "output": "Direction",
+  "colour": 230,
+  "tooltip": "Returns the direction (North, East, West, South) the rover is facing to.",
+  "helpUrl": ""
+},
+{
+  "type": "scan",
+  "message0": "Scan",
+  "output": "Boolean",
+  "colour": 230,
+  "tooltip": "Scans for an obstruction within the set scanning distance.",
+  "helpUrl": ""
+}]
