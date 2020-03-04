4b6900a916c5da842c412c43f7e31833e4f61fcc
Nearchos Paspallis <npaspallis@uclan.ac.uk>
2020-03-04 10:47:03 +0200

    Last touches - updates to UI - show code and execution trace - posting score at the end

--- android/app/src/main/java/io/github/nearchos/rovercoder/MainActivity.java
+++ android/app/src/main/java/io/github/nearchos/rovercoder/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.nearchos.rovercoder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,28 +14,33 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Vector;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import io.github.nearchos.rovercoder.api.APIConstants;
 import robutev3.android.BrickService;
 import robutev3.android.Device;
 import robutev3.android.EV3Service;
@@ -52,9 +58,9 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
     private ToggleButton toggleButton;
     private Button runNextCodeButton;
-    private Button viewCodeButton;
-    private TextView currentUserTextView;
-    private TextView scoreTextView;
+    private TextView executedCodeTextView;
+    private Spinner totalStepsSpinner;
+    private TextView javaScriptCodeTextView;
 
     private RequestQueue queue;
 
@@ -69,7 +75,6 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
         toggleButton = findViewById(R.id.toggleButton);
         runNextCodeButton = findViewById(R.id.runNextCodeButton);
-        viewCodeButton = findViewById(R.id.viewCodeButton);
 
         toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> updateRoverState(isChecked));
 
@@ -78,26 +83,75 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
         queue = Volley.newRequestQueue(this);
 
         runNextCodeButton.setOnClickListener(view -> {
-            queue.add(stringRequest); // Add the request to the RequestQueue.
-        });
-
-        viewCodeButton.setOnClickListener(view -> {
             new AlertDialog.Builder(this)
-                    .setTitle(R.string.View_code)
-                    .setMessage(javaScriptCode)
-                    .setPositiveButton("Dismiss", (dialog, which) -> dialog.dismiss())
+                    .setTitle("Please confirm")
+                    .setMessage("Are you sure you want to download and run the next player's code?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        getNextCode();
+                        dialog.dismiss();
+                    })
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
                     .create()
                     .show();
         });
 
-        currentUserTextView = findViewById(R.id.currentUserTextView);
-        currentUserTextView.setText(getString(R.string.Current_User, getString(R.string.None)));
+        javaScriptCodeTextView = findViewById(R.id.javaScriptCodeTextView);
+        javaScriptCodeTextView.setMovementMethod(new ScrollingMovementMethod());
 
-        scoreTextView = findViewById(R.id.scoreTextView);
-        scoreTextView.setText(getString(R.string.Score, 0));
+        executedCodeTextView = findViewById(R.id.executedCodeTextView);
+        executedCodeTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        totalStepsSpinner = findViewById(R.id.totalStepsSpinner);
+        totalStepsSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Step.values()));
+        totalStepsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                MainActivity.this.stepsTotal = Step.values()[position].getNumberOfSteps();
+                updateUI();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // ignore
+            }
+        });
+        totalStepsSpinner.setSelection(4);
 
         registerToEv3Events();
     }
+
+    public enum Step {
+        STEP_1(1),
+        STEP_5(5),
+        STEP_10(10),
+        STEP_30(30),
+        STEP_60(60),
+        STEP_100(100),
+        STEP_120(120),
+        ;
+
+        private int numberOfSteps;
+
+        Step(int numberOfSteps) {
+            this.numberOfSteps = numberOfSteps;
+        }
+
+        int getNumberOfSteps() {
+            return numberOfSteps;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return String.format(Locale.US, "%3d steps", numberOfSteps);
+        }
+    }
+
+    private String playerName = "None";
+    private long entryId = 0L;
+    private String javaScriptCode = JAVASCRIPT_CODE;
+    private int stepsCurrent = 0;
+    private int stepsTotal = 60;
 
     @Override
     protected void onStart() {
@@ -134,6 +188,7 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
     protected void onResume() {
         super.onResume();
         // set initial JavaScript code - todo must remove after the code is 'picked' from server
+        playerName = getString(R.string.None);
         setJavaScript(JAVASCRIPT_CODE);
         // create periodic handler
         scheduledWorkExecutor.scheduleAtFixedRate(codeExecutionRunnable, 0L, EXECUTION_INTERVAL_MILLISECONDS, TimeUnit.MILLISECONDS);
@@ -193,19 +248,17 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
     public static final int DEFAULT_SPEED = 20;
 
     public void updateRoverState(final boolean started) {
+        this.started = started;
         if(started) {
-            // beep
+            // beep to indicate started
             brickService.brick().sound().beep();
             // pulse red light when ready
             brickService.brick().light().red().pulse();
-            // start motors moving forward
-            brickService.brick().motor().portsBandC().turnIndefinitely(DEFAULT_SPEED).go();
         } else {
             brickService.brick().sound().beep(50, 100, 10);
             brickService.brick().light().red().on();
             brickService.brick().motor().stopAll().coast().go();
         }
-        this.started = started;
     }
 
     public void goForward(View view) {
@@ -230,6 +283,7 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
     public void moveForward(final int ms) {
         Log.d(TAG, "MOVE FORWARD");
+        codeExecutionTrace.append(stepsCurrent + ": FORWARD " + ms + " ms\n");
         brickService.brick().tank().forward(Interval.milliseconds(ms), Speed.fromValue(DEFAULT_SPEED)).go();
     }
 
@@ -239,6 +293,7 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
     public void moveBackward(final int ms) {
         Log.d(TAG, "MOVE BACKWARD");
+        codeExecutionTrace.append(stepsCurrent + ": BACKWARD " + ms + " ms\n");
         brickService.brick().tank().backward(Interval.milliseconds(500), Speed.fromValue(DEFAULT_SPEED)).go();
     }
 
@@ -248,6 +303,7 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
     public void turnLeft(final int ms) {
         Log.d(TAG, "TURN LEFT (" + ms + ")");
+        codeExecutionTrace.append(stepsCurrent + ": TURN LEFT " + ms + " ms\n");
         brickService.brick().tank().turnLeft(Speed.fromValue(DEFAULT_SPEED), Interval.milliseconds(ms)).go();
     }
 
@@ -257,12 +313,14 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
     public void turnRight(final int ms) {
         Log.d(TAG, "TURN RIGHT (" + ms + ")");
+        codeExecutionTrace.append(stepsCurrent + ": TURN RIGHT " + ms + " ms\n");
         brickService.brick().tank().turnRight(Speed.fromValue(DEFAULT_SPEED), Interval.milliseconds(ms)).go();
     }
 
     public int distance() {
         Log.d(TAG, "DISTANCE ...");
         final int distance = brickService.brick().sensor().port4().ultrasonic().centimeters();
+        codeExecutionTrace.append(stepsCurrent + ": DISTANCE() -> " + distance + " mm\n");
         Log.d(TAG, "DISTANCE -> " + distance + " mm");
         return distance;
     }
@@ -272,6 +330,7 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
         Log.d(TAG, "COLOR ...");
         final Color color = brickService.brick().sensor().port3().color().sense();
         if(color == Color.RED) brickService.brick().light().red().flash();
+        codeExecutionTrace.append(stepsCurrent + ": COLOR() -> " + color.name() + "\n");
         Log.d(TAG, "COLOR -> " + color.name());
         return color;
     }
@@ -337,11 +396,11 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
         scope = rhinoContext.initStandardObjects();
     }
 
-    private String javaScriptCode = JAVASCRIPT_CODE;
-
     public void setJavaScript(String javascriptCode) {
         this.javaScriptCode = javascriptCode;
         this.score = 0;
+        this.stepsCurrent = 0;
+        this.codeExecutionTrace.setLength(0); // clear trace
         rhinoContext.evaluateString(scope, javascriptCode, RUN_FUNCTION, 1, null);
         initFunction = (org.mozilla.javascript.Function) scope.get(INIT_FUNCTION, scope);
         final RoverCoderInterface roverCoderInterface = this;
@@ -352,6 +411,7 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
                 new Object[] {roverCoderInterface}
         );
         runFunction = (org.mozilla.javascript.Function) scope.get(RUN_FUNCTION, scope);
+        updateUI();
     }
 
     public void stopJavaScript() {
@@ -366,6 +426,8 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
 
         final RoverCoderInterface roverCoderInterface = this;
 
+        Log.d(TAG, "started: " + started + ", runFunction: " + runFunction + ", score: " + score + ", stepsCurrent: " + stepsCurrent + ", stepsTotal: " + stepsTotal);
+
         if(started && runFunction != null) {
             Log.d(TAG, "Running JS code ...");
             brickService.brick().light().green().pulse();
@@ -375,29 +437,91 @@ public class MainActivity extends AppCompatActivity implements RoverCoderInterfa
                     org.mozilla.javascript.Context.enter().initStandardObjects(),
                     new Object[] {roverCoderInterface}
             );
-            if(brickService.brick().sensor().port3().color().sense() == Color.RED) {
+            if(brickService.brick().sensor().port3().color().sense() == Color.GREEN) {
                 score++;
-                scoreTextView.setText(getString(R.string.Score, score));
+            }
+
+            stepsCurrent++;
+            // check if session finished: stepsCurrent > stepsTotal
+            if(stepsCurrent > stepsTotal) {
+                runOnUiThread(() -> {
+                    updateUI();
+                    postScore(entryId, score); // upload score
+                    stopJavaScript();
+                }); // as this is a separate thread, all UI actions must be done in a synchronized manner
             }
         }
+
+        runOnUiThread(this::updateUI); // as this is a separate thread, all UI actions must be done in a synchronized manner
     };
 
+    private StringBuilder codeExecutionTrace = new StringBuilder();
+
+    private void updateUI() {
+        javaScriptCodeTextView.setText(javaScriptCode);
+        executedCodeTextView.setText(playerName + ": " + score + "\n\n" + codeExecutionTrace.toString());
+    }
+
+    // Volley / Networking code
+
+    public static final String API_BASE_URL = "https://rovercoder.appspot.com/api";
+    public static final String GET_NEXT_CODE_URL = API_BASE_URL + "/getNextCode";
+    public static final String POST_SCORE_URL = API_BASE_URL + "/updateScore?entryID=%1&score=%2";
+
     // Request a string response from the provided URL.
-    private final StringRequest stringRequest = new StringRequest(Request.Method.GET, APIConstants.GET_NEXT_CODE_URL,
-            jsonResponse -> {
-                final com.panickapps.response.Response responseJsonObject = new Gson().fromJson(jsonResponse, com.panickapps.response.Response.class);
-
-                if (responseJsonObject.getData() != null) {
-                    if (responseJsonObject.getData().has("code")) {
-                        String code = responseJsonObject.getData().getAsJsonObject("code").get("code").getAsString();
-                        Log.d(MainActivity.TAG, code);
-                        setJavaScript(code);
-                        currentUserTextView.setText(getString(R.string.Current_User, responseJsonObject.getData().getAsJsonObject("code").get("playerName").getAsString()));
-
-                    }
-                }
-                Toast.makeText(MainActivity.this, responseJsonObject.getMessage(), Toast.LENGTH_LONG).show();
-            },
-            error -> Toast.makeText(MainActivity.this, "I/O Error: " + error, Toast.LENGTH_SHORT).show()
+    private final StringRequest stringRequest = new StringRequest(
+            Request.Method.GET,
+            GET_NEXT_CODE_URL,
+            this::handleJsonResponse,
+            this::handleError
     );
+
+    private void handleJsonResponse(final String jsonResponse) {
+        final com.panickapps.response.Response responseJsonObject = new Gson().fromJson(jsonResponse, com.panickapps.response.Response.class);
+
+        if (responseJsonObject.getData() != null) {
+            if (responseJsonObject.getData().has("code")) {
+                String code = responseJsonObject.getData().getAsJsonObject("code").get("code").getAsString();
+                Log.d(MainActivity.TAG, code);
+                playerName =  responseJsonObject.getData().getAsJsonObject("code").get("playerName").getAsString();
+                entryId = responseJsonObject.getData().getAsJsonObject("code").get("id").getAsLong();
+                setJavaScript(code);
+            }
+        }
+        Toast.makeText(MainActivity.this, responseJsonObject.getMessage(), Toast.LENGTH_LONG).show();
+    }
+
+    private void handleScoreJsonResponse(final String jsonResponse) {
+        Log.d(TAG, "POST score response -> " + jsonResponse);
+        final com.panickapps.response.Response responseJsonObject = new Gson().fromJson(jsonResponse, com.panickapps.response.Response.class);
+        Toast.makeText(MainActivity.this, responseJsonObject.getMessage(), Toast.LENGTH_LONG).show();
+        updateUI();
+    }
+
+    private void handleError(final VolleyError error) {
+        Toast.makeText(MainActivity.this, "I/O Error: " + error.getMessage() + " (" + error.networkResponse + ")", Toast.LENGTH_SHORT).show();
+    }
+
+    private void getNextCode() {
+        queue.add(stringRequest); // Add the request to the RequestQueue
+    }
+
+    private void postScore(final long entryId, final int score) {
+        Log.d(TAG, "Posting score: " + entryId + " -> " + score);
+        if(entryId == 0L) {
+            Log.d(TAG, "Ignoring score for entryId==0... " + score);
+            Toast.makeText(MainActivity.this, "Ignoring score for entryId==0... " + score, Toast.LENGTH_SHORT).show();
+        } else {
+            Log.d(TAG, "Posting score for player '" + playerName + "' (" + entryId + ") ... " + score);
+            Toast.makeText(MainActivity.this, "Posting score for player '" + playerName + "' (" + entryId + ") ... " + score, Toast.LENGTH_SHORT).show();
+            final String url = POST_SCORE_URL.replaceAll("%1", Long.toString(entryId)).replaceAll("%2", Integer.toString(score));
+            Log.d(TAG, "POSTing request to: " + url);
+            codeExecutionTrace.append("Posting score: ").append(score);
+            queue.add(new StringRequest(
+                    Request.Method.POST,
+                    url,
+                    this::handleScoreJsonResponse,
+                    this::handleError));
+        }
+    }
 }
--- android/app/src/main/java/io/github/nearchos/rovercoder/api/APIConstants.java
+++
@@ -1,8 +0,0 @@
-package io.github.nearchos.rovercoder.api;
-
-public class APIConstants {
-
-    public static final String API_BASE_URL = "https://rovercoder.appspot.com/api";
-    public static final String GET_NEXT_CODE_URL = API_BASE_URL + "/getNextCode";
-
-}
